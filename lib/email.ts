fbd4b81d0fa432c431e2cb0ff9ec91839aadb071
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-05-01 04:10:49 -0700

    in dash b

--- lib/email.ts
+++ lib/email.ts
@@ -54,7 +54,7 @@ export async function sendEmail({ to, subject, text, html }: EmailOptions) {
 
     // Create a transporter
     const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_SERVER_HOST || "smtp.ethereal.email",
+      host: process.env.EMAIL_SERVER_HOST || "smtp.gmail.com",
       port: Number.parseInt(process.env.EMAIL_SERVER_PORT || "587"),
       secure: process.env.EMAIL_SERVER_SECURE === "true",
       auth: {
